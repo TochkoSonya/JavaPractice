0ad5ae949caad9588cc01c3d385cc3641da558ab
TochkoSonya <sonya.tochko@live.com>
2021-08-22 19:57:28 +0300

    add Object

--- Algorithms/src/com/algorithm/Main.java
+++ Algorithms/src/com/algorithm/Main.java
@@ -8,23 +8,24 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-//        int n=25;
-//        Fibonacci fibonacci = new Fibonacci();
-//        fibonacci.timeWithFor(n);
-//        fibonacci.timeWithRecursive(n);
+        //fibonacci
+        int n=25;
+        Fibonacci fibonacci = new Fibonacci();
+        fibonacci.timeWithFor(n);
+        fibonacci.timeWithRecursive(n);
 
         //array invert
-        int[] array1= {1,2,3,4,5,6};
-        int[] array2= {1,2,3,4,5};
-        int[] array3= {1,2,3,4,5,6,7};
+        Object[] array1= {1,2,3,4,5,6};
+        Object[] array2= {1,2,3,4,5};
+        String[] array3= {"q","w","e"};
 
-        int[] result1 = ArrayReverse.reverseWithFor(array1);
+        Object[] result1 = ArrayReverse.reverseWithFor(array1);
         System.out.println(Arrays.toString(result1));
 
-        int[] result2 = ArrayReverse.reverseWithArray(array2);
+        Object[] result2 = ArrayReverse.reverseWithArray(array2);
         System.out.println(Arrays.toString(result2));
 
-        int[] result3 = ArrayReverse.reverseWithStream(array3);
+        Object[] result3 = ArrayReverse.reverseWithStream(array3);
         System.out.println(Arrays.toString(result3));
 
 
--- Algorithms/src/com/algorithm/reverse_array/ArrayReverse.java
+++ Algorithms/src/com/algorithm/reverse_array/ArrayReverse.java
@@ -3,32 +3,26 @@ package com.algorithm.reverse_array;
 import java.util.stream.IntStream;
 
 public class ArrayReverse {
-    public static int[] reverseWithFor(int[] array) {
+    public static Object[] reverseWithFor(Object[] array) {
         for(int i=0; i<array.length/2; i++) {
-            int temp=array[i];
+            Object temp=array[i];
             array[i] = array[array.length - i -1];
             array[array.length - i -1] = temp;
         }
         return array;
     }
 
-    public static int[] reverseWithArray(int[] array) {
-        int[] result=new int[array.length];
+    public static Object[] reverseWithArray(Object[] array) {
+        Object[] result=new Object[array.length];
         for(int i = array.length - 1, j=0; i>=0; i--, j++) {
             result[j]=array[i];
         }
         return result;
     }
 
-//    Object[] invertUsingStreams(Object[] array) {
-//        return IntStream.rangeClosed(1, array.length)
-//                .mapToObj(i -> array[array.length - i])
-//                .toArray();
-//    }
-
-    public static int[] reverseWithStream(int[] array) {
+    public static Object[] reverseWithStream(Object[] array) {
         return IntStream.rangeClosed(1, array.length)
-                .map(i -> array[array.length - i])
+                .mapToObj(i -> array[array.length - i])
                 .toArray();
     }
 }
